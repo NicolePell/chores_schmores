7d85cd8921d87d36304217db0b55de7695366ada
NicolePell <pellicena@gmail.com>
2016-06-07 07:27:20 +0100

    adds stub User data

--- lib/chores_schmores/repo.ex
+++ lib/chores_schmores/repo.ex
@@ -1,3 +1,29 @@
 defmodule ChoresSchmores.Repo do
-  use Ecto.Repo, otp_app: :chores_schmores
+  @moduledoc """
+  In memory repository.
+  """
+
+  def all(ChoresSchmores.User) do
+    [
+      %ChoresSchmores.User{id: "1", name: "Nicole", username: "nicpell", password: "elixir"},
+      %ChoresSchmores.User{id: "2", name: "Gareth", username: "predders", password: "elixir"},
+      %ChoresSchmores.User{id: "3", name: "Raquel", username: "raq", password: "elixir"},
+      %ChoresSchmores.User{id: "4", name: "Chris", username: "chrees", password: "elixir"}
+    ]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module),
+    fn map ->
+      Enum.all?(params, fn {key, val} ->
+        Map.get(map, key) == val end)
+    end
+  end
+
 end
---
+++ web/models/user.ex
@@ -0,0 +1,3 @@
+defmodule ChoresSchmores.User do
+  defstruct [:id, :name, :username, :password]
+end
